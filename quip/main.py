bc242e2b948f6ea895e80cc80e2435cf10b78cfb
amoh-godwin <amohgyebigodwin@gmail.com>
2021-08-20 12:56:27 +0000

    Update main.py

--- quip/main.py
+++ quip/main.py
@@ -1,14 +1,23 @@
 from typing import List
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-import gif_drive
-from dependencies import gif_drive
+import gif_access
+# from dependencies import gif_drive
 
 
 app =  FastAPI()
-app.include_router(gif_drive.router)
+app.include_router(gif_access.router)
 
 @app.get('/')
 def root():
     return {'root': "API not accessible as this"}
+
+
+@app.get('/upload/gif')
+def upload_gif():
+    with open('static/index.html') as index:
+        conts = index.read()
+
+    return HTMLResponse(content=conts)
