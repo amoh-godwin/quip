fd81d187800f5de509b58375b6306b2c79679abb
amoh-godwin <amohgyebigodwin@gmail.com>
2021-08-20 12:37:03 +0000

    Update main.py

--- quip/main.py
+++ quip/main.py
@@ -2,9 +2,12 @@ from typing import List
 
 from fastapi import FastAPI
 
+import gif_drive
+from dependencies import gif_drive
+
 
 app =  FastAPI()
-
+app.include_router(gif_drive.router)
 
 @app.get('/')
 def root():
